92cef6aef23f345692814bcf5eafdf61f8e07f10
unknown <ehuds@Ehuds-PC.dm.cx>
2015-11-24 14:52:24 +0200

    Fix default value of jobscanduration

--- src/main/java/com/checkmarx/jenkins/CxScanBuilder.java
+++ src/main/java/com/checkmarx/jenkins/CxScanBuilder.java
@@ -786,6 +786,10 @@ public class CxScanBuilder extends Builder {
         }
 
         public int getScanTimeoutDuration() {
+            if (scanTimeoutDuration < 1) {
+                scanTimeoutDuration  = 1;
+            }
+
             return scanTimeoutDuration;
         }
 
