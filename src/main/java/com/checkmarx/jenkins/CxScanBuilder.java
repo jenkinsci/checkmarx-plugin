416fb6df0e192e9096c080d9dc43e7a561808c06
CxGalnus <gal.nussbaum@checkmarx.com>
2018-12-19 11:50:09 +0200

    fix default preset - HF 8.80.3

--- src/main/java/com/checkmarx/jenkins/CxScanBuilder.java
+++ src/main/java/com/checkmarx/jenkins/CxScanBuilder.java
@@ -748,7 +748,7 @@ public class CxScanBuilder extends Builder implements SimpleBuildStep {
         ret.setSastEnabled(this.sastEnabled == null || sastEnabled); //for backward compatibility, assuming if sastEnabled is not set, then sast is enabled
 
         if (ret.getSastEnabled()) {
-            int presetId = parseInt(preset, log, "Invalid presetId: [%s]. Using default preset.", 0);
+            int presetId = parseInt(preset, log, "Invalid presetId: [%s]. Using default preset.", 1);
             ret.setPresetId(presetId);
 
             String excludeFolders = isGlobalExclusions() ? descriptor.getExcludeFolders() : getExcludeFolders();
