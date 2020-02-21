f0682a67e62a6182d4aedf350592bf6cd7edad6a
ilandn <ilandayans@gmail.com>
2020-02-21 11:22:13 -0600

    bugid: Fixed NPE in some edge cases CR_by: n/a

--- src/main/java/com/checkmarx/jenkins/CxScanCallable.java
+++ src/main/java/com/checkmarx/jenkins/CxScanCallable.java
@@ -2,8 +2,6 @@ package com.checkmarx.jenkins;
 
 import com.cx.restclient.CxShragaClient;
 import com.cx.restclient.configuration.CxScanConfig;
-import com.cx.restclient.dto.DependencyScanResults;
-import com.cx.restclient.dto.DependencyScannerType;
 import com.cx.restclient.dto.ScanResults;
 import com.cx.restclient.exception.CxClientException;
 import com.cx.restclient.osa.dto.OSAResults;
