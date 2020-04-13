1a4464704bb08c7536f745b217e1ff3ce3e6fef5
morad1992 <55655463+morad1992@users.noreply.github.com>
2020-04-13 18:49:27 +0300

    BUG FIX 627

--- src/main/java/com/checkmarx/jenkins/CxScanBuilder.java
+++ src/main/java/com/checkmarx/jenkins/CxScanBuilder.java
@@ -713,21 +713,20 @@ public class CxScanBuilder extends Builder implements SimpleBuildStep {
         File checkmarxBuildDir = new File(run.getRootDir(), "checkmarx");
         checkmarxBuildDir.mkdir();
 
-     if (config.getGeneratePDFReport()) {
+      if (config.getGeneratePDFReport()) {
 
             // run.getUrl() returns a URL path similar to job/MyJobName/124/
             String baseUrl=Jenkins.getInstance().getRootUrl();
             URL parsedUrl= new URL(baseUrl);
             String path= parsedUrl.getFile();
-            if(path==null) {
-                String pdfUrl = String.format(PDF_URL_TEMPLATE, run.getUrl());
-                scanResults.getSastResults().setSastPDFLink(pdfUrl);
+            if(!(path.equals("/"))) {
+                Path pdfUrlPath = Paths.get(path, run.getUrl(), "checkmarx/pdfReport");
+                scanResults.getSastResults().setSastPDFLink(pdfUrlPath.toString());
             }
             else {
-                Path pdfUrl = Paths.get(path, run.getUrl(), "checkmarx/pdfReport");
-                scanResults.getSastResults().setSastPDFLink(pdfUrl.toString());
+                Path pdfUrlPath = Paths.get(run.getUrl(), "checkmarx/pdfReport");
+                scanResults.getSastResults().setSastPDFLink(pdfUrlPath.toString());
             }
-      
         }
 
         //in case of async mode, do not create reports (only the report of the latest scan)
