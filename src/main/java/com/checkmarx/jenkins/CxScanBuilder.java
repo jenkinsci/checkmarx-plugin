5111db0239292e3c8bee20fc72fd40ad892b635a
Majd <majd.mahajenah@checkmarx.com>
2020-05-28 12:43:57 +0300

    revert changes of credentials library non exploitable vulnerabilities

--- src/main/java/com/checkmarx/jenkins/CxScanBuilder.java
+++ src/main/java/com/checkmarx/jenkins/CxScanBuilder.java
@@ -17,6 +17,7 @@ import com.cx.restclient.sast.dto.Preset;
 import com.cx.restclient.sast.dto.Project;
 import com.cx.restclient.sast.dto.SASTResults;
 import com.cx.restclient.sca.dto.SCAConfig;
+import com.cx.restclient.sca.dto.SCAResults;
 import com.cx.restclient.sca.dto.SourceLocationType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import freemarker.template.TemplateException;
@@ -68,9 +69,13 @@ public class CxScanBuilder extends Builder implements SimpleBuildStep {
 
 
     public static final String SCAN_REPORT_XML = "ScanReport.xml";
-    public static final String OSA_SUMMERY_JSON = "OSASummery.json";
+    public static final String OSA_SUMMERY_JSON = "OSASummary.json";
     public static final String OSA_LIBRARIES_JSON = "OSALibraries.json";
     public static final String OSA_VULNERABILITIES_JSON = "OSAVulnerabilities.json";
+
+    public static final String SCA_SUMMERY_JSON = "SCASummary.json";
+    public static final String SCA_LIBRARIES_JSON = "SCALibraries.json";
+    public static final String SCA_VULNERABILITIES_JSON = "SCAVulnerabilities.json";
 
     private static final String PDF_URL_TEMPLATE = "/%scheckmarx/pdfReport";
     private static final String PDF_URL = "checkmarx/pdfReport";
@@ -766,10 +771,12 @@ public class CxScanBuilder extends Builder implements SimpleBuildStep {
                 cxScanResult.setSastResults(sastResults);
             }
 
-            //create osa reports
+            //create osa and sca reports
             DependencyScanResults dsResults = scanResults.getDependencyScanResults();
             if (dsResults != null && dsResults.getOsaResults() != null && dsResults.getOsaResults().isOsaResultsReady()) {
                 createOsaReports(dsResults.getOsaResults(), checkmarxBuildDir);
+            }else if(dsResults != null && dsResults.getScaResults() != null && dsResults.getScaResults().isScaResultReady()){
+                createScaReports(dsResults.getScaResults(),checkmarxBuildDir);
             }
             return;
         }
@@ -1009,6 +1016,12 @@ public class CxScanBuilder extends Builder implements SimpleBuildStep {
         writeJsonObjectToFile(osaResults.getResults(), new File(checkmarxBuildDir, OSA_SUMMERY_JSON), "OSA summery json report");
         writeJsonObjectToFile(osaResults.getOsaLibraries(), new File(checkmarxBuildDir, OSA_LIBRARIES_JSON), "OSA libraries json report");
         writeJsonObjectToFile(osaResults.getOsaVulnerabilities(), new File(checkmarxBuildDir, OSA_VULNERABILITIES_JSON), "OSA vulnerabilities json report");
+    }
+
+    private void createScaReports(SCAResults scaResults,File checkmarxBuildDir) {
+        writeJsonObjectToFile(scaResults.getSummary(), new File(checkmarxBuildDir, SCA_SUMMERY_JSON), "OSA summary json report");
+        writeJsonObjectToFile(scaResults.getPackages(), new File(checkmarxBuildDir, SCA_LIBRARIES_JSON), "OSA libraries json report");
+        writeJsonObjectToFile(scaResults.getFindings(), new File(checkmarxBuildDir, SCA_VULNERABILITIES_JSON), "OSA vulnerabilities json report");
     }
 
     private String generateHTMLReport(@Nonnull FilePath workspace, File checkmarxBuildDir, CxScanConfig config, ScanResults results) {
