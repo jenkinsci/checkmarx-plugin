13854306fe57e955147f764b32c45a43318d7a36
Subhadra.Sahoo@checkmarx.com <subhadra_sahoo@persistent.com>
2021-02-09 21:14:29 +0530

    Addressed the issue for cxOrigin where in the Jenkins jobs were failing because they have job name contains alpha numeric characters #AB1419

--- src/main/java/com/checkmarx/jenkins/CxScanBuilder.java
+++ src/main/java/com/checkmarx/jenkins/CxScanBuilder.java
@@ -1152,6 +1152,7 @@ public class CxScanBuilder extends Builder implements SimpleBuildStep {
         try {
             String jobName = env.get("JOB_NAME");
             jobName = URLDecoder.decode(jobName, "UTF-8");
+            jobName = jobName.replaceAll("[^.a-zA-Z0-9\\s]", " ");
             String jenURL = env.get("JENKINS_URL");
             jenURL = jenURL.substring((jenURL.lastIndexOf("://")) + 3);
             String hostName = "";
@@ -1160,13 +1161,14 @@ public class CxScanBuilder extends Builder implements SimpleBuildStep {
             } else {
                 hostName = jenURL;
             }
-            passedURL = hostName + " " + jobName;
+            passedURL = "Jenkins " + hostName + " " + jobName;
+            // 50 is the maximum number of characters allowed by SAST server
             if(passedURL.length()>50)
                 passedURL=passedURL.substring(0,50);
             else
                 passedURL=passedURL;
         } catch (UnsupportedEncodingException e) {
-            log.error("Failed to get jwnkin URL of the JOB: " + e.getMessage());
+            log.error("Failed to get Jenkins URL of the JOB: " + e.getMessage());
         }
         return passedURL;
     }
@@ -1178,7 +1180,7 @@ public class CxScanBuilder extends Builder implements SimpleBuildStep {
         try {
             jobName = URLDecoder.decode(jobName, "UTF-8");
         } catch (UnsupportedEncodingException e) {
-            log.error("Failed to get jenkin URL of the JOB: " + e.getMessage());
+            log.error("Failed to get name of the Jenkins Job: " + e.getMessage());
         }
 
         String originUrl = jenURL+"job/"+jobName;
